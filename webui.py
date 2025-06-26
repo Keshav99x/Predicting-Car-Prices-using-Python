6f7e213860b1484cf5bdd2180846087dc4b5437b
Keshav99x <104844948+Keshav99x@users.noreply.github.com>
2025-06-26 09:10:59 +0530

    Update webui.py

--- webui.py
+++ webui.py
@@ -25,15 +25,15 @@ iface = gr.Interface(
     inputs=[
         gr.Radio([0, 1], label="Leather Interior (0=No, 1=Yes)"),
         gr.Dropdown([0, 1, 2], label="Fuel Type (0=Hybrid, 1=Petrol, 2=Diesel)"),
-        gr.Number(label="Engine Volume (e.g. 2.0)"),
+        gr.Number(label="Engine Volume in litres(e.g. 2.0)"),
         gr.Number(label="Mileage (in km)"),
         gr.Number(label="Number of Cylinders"),
-        gr.Dropdown([0, 1, 2, 3], label="Gear Box Type (e.g. 0=Auto, 1=Tiptronic, 2=Manual, 3=Variator)"),
+        gr.Dropdown([0, 1, 2, 3], label="Gear Box Type (0=Auto, 1=Tiptronic, 2=Manual, 3=Variator)"),
         gr.Dropdown([0, 1, 2], label="Drive Wheels (0=Front, 1=4x4, 2=Rear)"),
         gr.Number(label="Airbags"),
         gr.Number(label="Car Age (in years)"),
     ],
-    outputs="text",
+    outputs=["text", label = "Predicted Car Price"]
     title="Car Price Prediction",
     description="Enter car features to estimate its market price"
 )
