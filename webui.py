091fff773d15c2eaf5199d9b56b3b38e726a1472
Keshav99x <104844948+Keshav99x@users.noreply.github.com>
2025-06-26 09:15:15 +0530

    Update webui.py

--- webui.py
+++ webui.py
@@ -3,10 +3,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from main import regressor
 
-# Use your trained model here
-# Make sure regressor is already trained (as in your notebook)
-# regressor = RandomForestRegressor(...)
-# regressor.fit(X_train, Y_train)
+
 
 # Prediction function
 def predict_price(leather_interior, fuel_type, engine_volume, mileage,
@@ -25,7 +22,7 @@ iface = gr.Interface(
     inputs=[
         gr.Radio([0, 1], label="Leather Interior (0=No, 1=Yes)"),
         gr.Dropdown([0, 1, 2], label="Fuel Type (0=Hybrid, 1=Petrol, 2=Diesel)"),
-        gr.Number(label="Engine Volume in litres(e.g. 2.0)"),
+        gr.Number(label="Engine Volume in litres (e.g. 2.0)"),
         gr.Number(label="Mileage (in km)"),
         gr.Number(label="Number of Cylinders"),
         gr.Dropdown([0, 1, 2, 3], label="Gear Box Type (0=Auto, 1=Tiptronic, 2=Manual, 3=Variator)"),
@@ -33,7 +30,7 @@ iface = gr.Interface(
         gr.Number(label="Airbags"),
         gr.Number(label="Car Age (in years)"),
     ],
-    outputs=["text", label = "Predicted Car Price"]
+    outputs="text",
     title="Car Price Prediction",
     description="Enter car features to estimate its market price"
 )
